d5d25d16089c97b1c508a932324e8f8ff1080097
Baldwin Kiel Malabanan <89601599+Baldwin2318@users.noreply.github.com>
2026-06-10 09:20:30 -0400

    Add wedding game API routes

---
+++ server/weddingGameRoutes.js
@@ -0,0 +1,8 @@
+const THREE_LETTER_WORDS = [
+  'ace', 'air', 'all', 'ant', 'ape', 'arc', 'arm', 'art', 'ash', 'awe',
+  'bag', 'bar', 'bat', 'bay', 'bee', 'big', 'bit', 'bow', 'box', 'boy',
+  'bud', 'bun', 'bus', 'cab', 'can', 'cap', 'cat', 'cop', 'cow', 'cup',
+  'dad', 'day', 'den', 'dew', 'dip', 'dog', 'dot', 'dry', 'ear', 'egg',
+  'elf', 'fan', 'far', 'fig', 'fin', 'fit', 'fog', 'fox', 'fun', 'gem',
+  'get', 'gig', 'gum', 'hat', 'hen', 'hip', 'hop', 'hug', 'ice', 'jam',
+  'jar', 'joy',
